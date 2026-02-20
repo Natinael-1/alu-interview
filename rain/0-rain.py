4c64340b9f5d75c511afeb06e5d151709db163b4
Natinael Boda Borana <n.borana@alustudent.com>
2026-02-20 10:26:31 +0000

    Edited

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""
-Module for calculating rainwater retention.
-"""
-
-
 def rain(walls):
-
     if not walls or len(walls) < 3:
         return 0
-
     water = 0
     left = 0
     right = len(walls) - 1
@@ -17,20 +10,15 @@ def rain(walls):
 
     while left < right:
         if walls[left] < walls[right]:
-           
             if walls[left] >= left_max:
                 left_max = walls[left]
             else:
-                # Water trapped = Max height seen - current height
                 water += left_max - walls[left]
             left += 1
         else:
-           
             if walls[right] >= right_max:
                 right_max = walls[right]
             else:
-                
                 water += right_max - walls[right]
             right -= 1
-
     return water
